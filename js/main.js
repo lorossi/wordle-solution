a1282fd5ca18ed649462c3391950695d683381d2
lorossi <contact@lorenzoros.si>
2022-02-01 08:51:01 +0100

    fixed layout

--- js/main.js
+++ js/main.js
@@ -11,26 +11,44 @@ const load_index = (timestamp = null) => {
 
 document.addEventListener("DOMContentLoaded", () => {
   // load index and word
-  const index = load_index();
-  const word = words[index].toUpperCase();
+
   // object selectors
-  const today_word_obj = document.querySelector("#today .word");
-  const tomorrow_obj = document.querySelector("#tomorrow");
-  // set text on today's selectors
-  today_word_obj.innerHTML = word;
-  document.title = `Today's Wordle is: ${word}`;
-  // listener for click on tomorrow
-  tomorrow_obj.addEventListener("click", () => {
-    // calculate tomorrow as today plus 1 day
-    const tomorrow_date = new Date(new Date());
-    tomorrow_date.setDate(tomorrow_date.getDate() + 1);
-    tomorrow_date.setHours(0, 0, 0, 0);
-    // load timestamp and word for tomorrow
-    const timestamp = tomorrow_date.getTime();
-    const index = load_index(timestamp);
-    const word = words[index].toUpperCase();
-    // set element
-    tomorrow_obj.innerHTML = word;
-    tomorrow_obj.style.animation = "FadeIn 1s";
+
+  document.querySelectorAll("#today, #tomorrow").forEach((s) => {
+    s.addEventListener("click", (e) => {
+      let timestamp;
+
+      switch (e.target.id) {
+        case "tomorrow":
+          const tomorrow_date = new Date(new Date());
+          tomorrow_date.setDate(tomorrow_date.getDate() + 1);
+          tomorrow_date.setHours(0, 0, 0, 0);
+          timestamp = tomorrow_date.getTime();
+          break;
+        case "today":
+          timestamp = null;
+          break;
+      }
+
+      const index = load_index(timestamp);
+      e.target.innerHTML = words[index].toUpperCase();
+      e.target.style.animation = "FadeIn 1s";
+    });
   });
+
+  // // listener for click on tomorrow
+  // selectors.addEventListener("click", (e) => {
+  //   console.log(word);
+  // calculate tomorrow as today plus 1 day
+  // const tomorrow_date = new Date(new Date());
+  // tomorrow_date.setDate(tomorrow_date.getDate() + 1);
+  // tomorrow_date.setHours(0, 0, 0, 0);
+  // load timestamp and word for tomorrow
+  // const timestamp = tomorrow_date.getTime();
+  // const index = load_index(timestamp);
+  // const word = words[index].toUpperCase();
+  // set element
+  // tomorrow_obj.innerHTML = word;
+  // tomorrow_obj.style.animation = "FadeIn 1s";
+  // });
 });
